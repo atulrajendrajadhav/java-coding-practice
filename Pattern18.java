8ffe399c81026ec022aab5fe5c106dc4f4850748
Atul Jadhav <jadhavatul3526@gmail.com>
2026-07-03 22:00:42 +0530

    Pattern Example

---
+++ Pattern18.java
@@ -0,0 +1,12 @@
+class Pattern18{
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=(n-n/2);j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
