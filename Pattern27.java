24689234d1a89c621590a8789a5126dbe13c4141
Atul Jadhav <jadhavatul3526@gmail.com>
2026-07-12 22:22:30 +0530

    Add files via upload

---
+++ Pattern27.java
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////
+///    2
+///   3 4
+///  4 5 6
+/// 5 6 7 8
+///6 7 8 9 10
+/// //////////////////////////////////////////////////////////////////////////
+public class Pattern27 {
+    public static void main(String[] args) {
+        int n =5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(  i+j +" ");
+            }
+            System.out.println();
+        }
+    }
+    }
+
